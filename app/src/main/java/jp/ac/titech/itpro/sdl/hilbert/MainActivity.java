0481718170b1c6024efca856988caeb92f5fd822
ke-sasada <sasada.k.ab@m.titech.ac.jp>
2016-05-02 17:59:38 +0900

    演習:イベントのモニタリング *change author name

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -2,6 +2,7 @@ package jp.ac.titech.itpro.sdl.hilbert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,18 +12,54 @@ public class MainActivity extends AppCompatActivity {
     private final static int MAX_N = 9;
     private int n = 1;
 
+    private static final String TAG = "Main Activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+
         setContentView(R.layout.activity_main);
         levelView = (TextView)findViewById(R.id.level_view);
         hilbertView = (HilbertView)findViewById(R.id.hilbert_view);
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG,"onResume");
+
         display();
+    }
+
+    @Override
+    protected  void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
     }
 
     public void onClickDec(View v) {
@@ -41,3 +78,4 @@ public class MainActivity extends AppCompatActivity {
         hilbertView.setLevel(n);
     }
 }
+
